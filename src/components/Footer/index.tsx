abf4150b92ab99c160bd1374130ef66e7dfbc564
Ewelina Sarad <ewelina.sarad@lunarlogic.io>
2021-11-12 15:31:40 +0100

    Style footer for different devices and add Row and column coponents"

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,55 +1,42 @@
 import React, { FC } from "react";
-import styled from "styled-components";
 
 import Logo from "../Logo";
 import P from "../P";
 import Icon from "../Icon";
+import Row from "../Row";
+import Column from "../Column";
 import Mail from "../../assets/img/icons/mail.svg";
-import { minDevice } from "../../assets/styles/breakpoints";
 import {
-  white,
-  darkGradientBackground,
-  copyrights,
-} from "../../assets/styles/colors";
+  FooterWrapper,
+  Wrapper,
+  ContactWrapper,
+  FooterP,
+  MailA,
+} from "./styledComponents";
+import { white, copyrights } from "../../assets/styles/colors";
 
-const FooterWrapper = styled.div`
-  width: 100%;
-  height: auto;
-  display: flex;
-  flex-direction: column;
-  padding: 1em;
-  background: ${darkGradientBackground};
-`;
-
-const ContactWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding-left: 2em;
-`;
-
-const Row = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-// interface FooterProps {}
-
-const Footer = () => {
+const Footer: FC = () => {
   return (
     <FooterWrapper>
-      <Logo isBackgroundDark width="137px" />
-      <ContactWrapper>
-        <P color={white}>Kontakt</P>
-        <Row>
-          <Icon color={white} width="1em">
-            <img src={Mail} />
-          </Icon>
-          <P color={white}>myslaceklasy@email.com</P>
-        </Row>
-      </ContactWrapper>
-      <P color={copyrights} textAlign="center">
-        © Copyrights 2020.
-      </P>
+      <Wrapper>
+        <Logo isBackgroundDark width="137px" />
+        <ContactWrapper>
+          <FooterP>Kontakt</FooterP>
+          <Row>
+            <Icon color={white} width="1em">
+              <img src={Mail} />
+            </Icon>
+            <MailA href="mailto:myslaceklasy@email.com">
+              myslaceklasy@email.com
+            </MailA>
+          </Row>
+        </ContactWrapper>
+      </Wrapper>
+      <Column>
+        <P color={copyrights} textAlign="center" fontSize="0.875em">
+          © Copyrights 2020.
+        </P>
+      </Column>
     </FooterWrapper>
   );
 };
---
+++ src/components/Footer/styledComponents.tsx
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+import P from "../P";
+import { minDevice } from "../../assets/styles/breakpoints";
+import { darkGradientBackground, white } from "../../assets/styles/colors";
+
+const FooterWrapper = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  background: ${darkGradientBackground};
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 1em;
+  margin: 1em;
+  @media ${minDevice.desktopTablet} {
+    flex-direction: row;
+  }
+`;
+
+const ContactWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 2em;
+`;
+
+const FooterP = styled(P)`
+  font-size: 0.875em;
+  font-weight: 700;
+  color: ${white};
+  @media ${minDevice.desktopTablet} {
+    font-size: 1.25em;
+  }
+  @media ${minDevice.desktopL} {
+    font-size: 1.5em;
+  }
+  @media ${minDevice.desktopXL} {
+    font-size: 2em;
+  }
+`;
+
+const MailA = styled.a`
+  font-size: 0.75em;
+  font-weight: 400;
+  color: ${white};
+  @media ${minDevice.desktopTablet} {
+    font-size: 1em;
+  }
+  @media ${minDevice.desktopL} {
+    font-size: 1.25em;
+  }
+  @media ${minDevice.desktopXL} {
+    font-size: 2em;
+  }
+`;
+
+export { FooterWrapper, Wrapper, ContactWrapper, FooterP, MailA };
