aea62f781812411f6d89e8098c3934d200ab3de2
bazelajoanna <51509583+bazelajoanna@users.noreply.github.com>
2021-11-12 12:38:49 +0100

    Add logo (#4)
    * Add logo image
    
    * Responsive layout for Logo component
    
    * Add logo for dark background
    
    * Remove unnecessary files from public folder
    
    * Add review changes
    
    * Fix paragraph typing
    
    * Fix theme condition in Logo

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+
+import { minDevice } from "../../assets/styles/breakpoints";
+import logoBlack from "../../assets/img/logo-black.svg";
+import logoWhite from "../../assets/img/logo-white.svg";
+
+const LogoDiv = styled.div`
+  width: 96px;
+  display: flex;
+
+  @media ${minDevice.desktopTablet} {
+    width: 158px;
+  }
+
+  @media ${minDevice.desktopL} {
+    width: 185px;
+  }
+`;
+
+const Img = styled.img`
+  width: 100%;
+`;
+
+interface LogoProps {
+  isBackgroundDark: boolean;
+}
+
+const Logo: FC<LogoProps> = ({ isBackgroundDark }) => {
+  return (
+    <LogoDiv>
+      <Img src={isBackgroundDark ? logoWhite : logoBlack} />
+    </LogoDiv>
+  );
+};
+
+export default Logo;
---
+++ src/components/P/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+interface PProps {
+  fontSize: string;
+  fontWeight: string;
+  color: string;
+  textAlign: string;
+  marginLeft: string;
+  marginBottom: string;
+  marginTop: string;
+  width: string;
+  shadow: string;
+}
+
+type PropsOptional = Partial<PProps>;
+
+const P = styled.p<PropsOptional>`
+  font-size: ${(props) => props.fontSize || "1em"};
+  font-weight: ${(props) => props.fontWeight || "400"};
+  color: ${(props) => props.color || "inherit"};
+  text-align: ${(props) => props.textAlign || "inherit"};
+  margin-left: ${(props) => props.marginLeft || "0"};
+  margin-bottom: ${(props) => props.marginBottom || "0"};
+  margin-top: ${(props) => props.marginTop || "0"};
+  width: ${(props) => props.width || "auto"};
+  text-shadow: ${(props) => props.shadow || "0"};
+`;
+
+export default P;
