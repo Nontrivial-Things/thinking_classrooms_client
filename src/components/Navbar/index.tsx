2dac965809886ca23217c23a9c8d253863c92b9e
bazelajoanna <51509583+bazelajoanna@users.noreply.github.com>
2021-11-16 15:38:33 +0100

    Add navbar (#7)
    * Create Navbar component with hamburger menu
    
    * Create file with styled components
    
    * Add eslint rule that enable debugger
    
    * Navbar refactor proposition
    
    * Change semantic in navigation
    
    * Remove NavbarClassType as it was not needed
    
    Co-authored-by: franiakat <frania.kat@gmail.com>

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,47 @@
+import React, { FC, useState } from 'react';
+
+import Logo from '../Logo';
+import { NavbarDiv, NavbarMenu, NavbarMenuList, NavbarMenuItem, HamburgerImg } from './styledComponents';
+import hamburger from '../../assets/img/icons/menu.svg';
+import hamburgerX from '../../assets/img/icons/x.svg';
+
+enum NavbarMenuState {
+  ACTIVE = 'active',
+  CLOSED = 'closed',
+}
+
+const NavbarClass = {
+  active: NavbarMenuState.ACTIVE,
+  closed: '',
+};
+
+const Navbar: FC = () => {
+  const [navbarMenuState, setNavbarMenuState] = useState<NavbarMenuState>(NavbarMenuState.CLOSED);
+
+  const isNavbarMenuActive = navbarMenuState === NavbarMenuState.ACTIVE;
+
+  const toggleHamburger = () => {
+    isNavbarMenuActive ? setNavbarMenuState(NavbarMenuState.CLOSED) : setNavbarMenuState(NavbarMenuState.ACTIVE);
+  };
+
+  const navbarMenuStyle = isNavbarMenuActive ? NavbarClass.active : NavbarClass.closed;
+
+  return (
+    <NavbarDiv className={navbarMenuStyle}>
+      <Logo isBackgroundDark={false}></Logo>
+      <HamburgerImg
+        className={navbarMenuStyle}
+        onClick={toggleHamburger}
+        src={isNavbarMenuActive ? hamburgerX : hamburger}
+        alt="Hamburger menu icon"
+      ></HamburgerImg>
+      <NavbarMenu>
+        <NavbarMenuList className={navbarMenuStyle}>
+          <NavbarMenuItem>O metodzie</NavbarMenuItem>
+          <NavbarMenuItem>Strefa moderatora</NavbarMenuItem>
+        </NavbarMenuList>
+      </NavbarMenu>
+    </NavbarDiv>
+  );
+};
+export default Navbar;
---
+++ src/components/Navbar/styledComponents.tsx
@@ -0,0 +1,110 @@
+import styled from "styled-components";
+
+import { minDevice } from "../../assets/styles/breakpoints";
+import { activeLink } from "../../assets/styles/colors";
+
+const NavbarDiv = styled.div`
+  height: 5em;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1.16em 0 0.81em;
+
+  &.active {
+    box-shadow: 0px 1px 10px rgba(0, 0, 0, 0.08);
+  }
+
+  @media ${minDevice.desktopTablet} {
+    margin: 0 1.5em 0 1.31em;
+    height: 4em;
+    box-shadow: none;
+  }
+
+  @media ${minDevice.desktopL} {
+    margin: 0 6.25em 0 6.06em;
+    height: 4.5em;
+  }
+
+  @media ${minDevice.desktopXL} {
+    margin: 0 12.31em 0 12.5em;
+  }
+`;
+const NavbarMenu = styled.nav`
+  transform: translateY(-150%);
+  position: absolute;
+
+  @media ${minDevice.desktopTablet} {
+    transform: translateY(-0%);
+    position: relative;
+  }
+`;
+
+const NavbarMenuList = styled.ol`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  height: 7em;
+  transform: translateY(-150%);
+  position: absolute;
+
+  &.active {
+    transform: translateY(-30%);
+    top: 80px;
+  }
+
+  @media ${minDevice.desktopTablet} {
+    flex-direction: row;
+    transform: translateY(-0%);
+    position: relative;
+    align-items: center;
+  }
+`;
+
+const NavbarMenuItem = styled.li`
+  font-size: 0.785em;
+  display: flex;
+  position: relative;
+  cursor: pointer;
+  padding-left: 0.69em;
+
+  @media ${minDevice.desktopTablet} {
+    display: block;
+    padding: 0;
+
+    &:nth-child(1) {
+      margin-right: 1.5em;
+    }
+    &:hover {
+      font-weight: 700;
+    }
+
+    &.active {
+      &::after {
+        height: 0.25em;
+        width: 100%;
+        background-color: ${activeLink};
+        border-radius: 0.625em 0.625em 0px 0px;
+        content: "";
+        position: absolute;
+        bottom: -1.25em;
+        left: 0;
+      }
+    }
+  }
+
+  @media ${minDevice.desktopL} {
+    font-size: 1em;
+    &:nth-child(1) {
+      margin-right: 2em;
+    }
+  }
+`;
+
+const HamburgerImg = styled.img`
+  width: 0.98em;
+  @media ${minDevice.desktopTablet} {
+    display: none;
+  }
+`;
+
+export { NavbarDiv, NavbarMenu, NavbarMenuList, NavbarMenuItem, HamburgerImg };
