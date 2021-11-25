98b5c18962db744e5fe81fd6c4ba506bb9e5dc6f
bazelajoanna <bazela.joanna@gmail.com>
2021-11-25 16:48:58 +0100

    Support responsive design for buttons

--- src/components/Button/styledComponents.tsx
+++ src/components/Button/styledComponents.tsx
@@ -7,6 +7,7 @@ import {
   hoverSecondaryButton,
 } from "../../assets/styles/colors";
 import { ReactComponent as Download } from "../../assets/img/icons/download.svg";
+import { minDevice } from "../../assets/styles/breakpoints";
 
 interface ButtonStyleProps {
   isPrimary: boolean;
@@ -16,13 +17,13 @@ const Button = styled.button<ButtonStyleProps>`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 16px 48px;
+  padding: 8px 32px;
   box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.15);
   border: none;
   border-radius: 5px;
   background-color: ${activeButton};
   font-family: "PT Sans", sans-serif;
-  font-size: 1rem;
+  font-size: 14px;
   font-weight: 700;
   color: #ffffff;
   letter-spacing: 0.04rem;
@@ -41,6 +42,11 @@ const Button = styled.button<ButtonStyleProps>`
     box-shadow: none;
   }
 
+  @media ${minDevice.desktopL} {
+    padding: 16px 48px;
+    font-size: 1rem;
+  }
+
   ${(props) =>
     !props.isPrimary &&
     css`
@@ -48,7 +54,6 @@ const Button = styled.button<ButtonStyleProps>`
       color: ${activeButton};
       font-weight: bold;
       border: 1.5px solid ${activeButton};
-      padding: 16px 32px;
       box-shadow: none;
 
       &:hover,
@@ -61,6 +66,10 @@ const Button = styled.button<ButtonStyleProps>`
         background-color: transparent;
         border-color: ${disabledButton};
         stroke: ${disabledButton};
+      }
+      @media ${minDevice.desktopL} {
+        padding: 16px 32px;
+        font-size: 1rem;
       }
     `}
 `;
