b609b4a32509aa5cfc54e68f8a08d52c2ddc2cdb
franiakat <frania.kat@gmail.com>
2021-12-13 16:01:36 +0100

    Put optional prop with defualt at the end of the props list in <Tag/>

--- src/components/atoms/Tag/index.tsx
+++ src/components/atoms/Tag/index.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 import * as S from "./styles";
 import { TagStyleProps } from "./interface";
 
-const Tag: FC<TagStyleProps> = ({ text, isDroppable = false, fontSize }) => {
+const Tag: FC<TagStyleProps> = ({ text, fontSize, isDroppable = false }) => {
   return (
     <S.TagWrapper fontSize={fontSize}>
       {text}
