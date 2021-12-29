003e1debfb55bba4476a6ffcd7cf607773e99c3e
Ewelina Sarad-Cichuta <evelinasarad@gmail.com>
2021-12-29 13:12:26 +0100

    Add mock data to production (#34)
    Co-authored-by: Ewelina Sarad <ewelina.sarad@lunarlogic.io>

--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,8 @@ import GlobalStyle from "./assets/styles/global-styles";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./apolloClient";
 
-if (process.env.NODE_ENV === "development") {
-  const { worker } = require("./mocks/browser");
-  worker.start();
-}
+const { worker } = require("./mocks/browser");
+worker.start();
 
 ReactDOM.render(
   <BrowserRouter>
