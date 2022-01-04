9d0987ab506c9ffff12062cda9728b6a2217c35d
bazelajoanna <bazela.joanna@gmail.com>
2022-01-04 16:38:50 +0100

    Add tests

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -3,11 +3,22 @@ describe("<App />", () => {
     cy.visit("http://localhost:3000/");
   });
 
-  it("shows problem extension after clicking on suggestion", async () => {
+  it("shows problem extension after clicking on suggestion", () => {
     cy.get(`a[href="/problems/1"]`).first().click();
     cy.contains("Treść problemu");
-    cy.get(".ScrollButton-sc-si0bqp-0").should("not.be.visible");
+  });
+
+  it("scrolls to the top of the page after clicking on scroll button", () => {
+    cy.get(`a[href="/problems/1"]`).first().click();
     cy.scrollTo(0, 400);
+
     cy.get(".ScrollButton-sc-si0bqp-0").should("be.visible").click();
+
+    cy.should("be.visible", "O metodzie");
+  });
+
+  it("shows download button", () => {
+    cy.get(`a[href="/problems/1"]`).first().click();
+    cy.contains("Pobierz treść");
   });
 });
