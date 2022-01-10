4b9b60209b290658a96719f87d17d9f682f67547
bazelajoanna <bazela.joanna@gmail.com>
2022-01-10 13:26:43 +0100

    Add "use" named import in i18n configuration

--- src/i18n.js
+++ src/i18n.js
@@ -1,14 +1,13 @@
-import i18n from "i18next";
+import { use } from "i18next";
 import { initReactI18next } from "react-i18next";
 
 import Backend from "i18next-http-backend";
 
-i18n
-  .use(Backend)
+const i18n = use(Backend)
   .use(initReactI18next)
   .init({
     fallbackLng: "pl",
-    debug: true,
+    debug: false,
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
