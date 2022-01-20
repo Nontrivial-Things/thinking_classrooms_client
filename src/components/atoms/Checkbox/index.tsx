727f99ff493643522a819a434dfa9f1d1a04c5ad
Ewelina Sarad <ewelina.sarad@lunarlogic.io>
2022-01-20 10:58:50 +0100

    Fix props in checkbox

--- src/components/atoms/Checkbox/index.tsx
+++ src/components/atoms/Checkbox/index.tsx
@@ -3,13 +3,15 @@ import { FC } from "react";
 import { CheckBoxProps } from "./interface";
 import * as S from "./styles";
 
-const Checkbox: FC<CheckBoxProps> = (checked) => (
-  <S.CheckboxContainer>
-    <S.HiddenCheckbox checked={checked} />
-    <S.StyledCheckbox checked={checked}>
-      <S.CheckIcon checked={checked}></S.CheckIcon>
-    </S.StyledCheckbox>
-  </S.CheckboxContainer>
-);
+const Checkbox: FC<CheckBoxProps> = (checked) => {
+  return (
+    <S.CheckboxContainer>
+      <S.HiddenCheckbox />
+      <S.StyledCheckbox>
+        <S.CheckIcon></S.CheckIcon>
+      </S.StyledCheckbox>
+    </S.CheckboxContainer>
+  );
+};
 
 export default Checkbox;
