e568055db9ea967f4fc5821ae13f5ea42c80dc90
Ewelina Sarad <ewelina.sarad@lunarlogic.io>
2022-01-31 17:03:34 +0100

    add test WIP

--- cypress/integration/login_page.js
+++ cypress/integration/login_page.js
@@ -17,4 +17,18 @@ describe("Problem Detailed Page", () => {
 
     cy.contains("Błędne hasło");
   });
+
+  it.only("doesn't logout user when she/he clicked 'Remember me' checbox", () => {
+    cy.get('input[name="email"]').type("kasia@tc.io");
+    cy.get('input[name="password"]').type("!Pass112233");
+    // cy.get('[type="checkbox"]').check({ force: true });
+
+    cy.get("form").submit();
+    cy.contains("Witaj, moderatorze!");
+
+    cy.clearLocalStorage(/user/);
+
+    cy.get('a[href*="/moderator"]').click();
+    cy.contains("Witaj, moderatorze!");
+  });
 });
