7922ca145a47a799e57ddd1cc7698ec1d53df2dc
Ewelina Sarad <ewelina.sarad@lunarlogic.io>
2022-02-06 15:20:57 +0100

    Merge changes

---
+++ pages/problems/[problemId].tsx
@@ -0,0 +1,226 @@
+import { FC, useEffect, useState, useRef } from "react";
+import { useRouter } from "next/router";
+import { useQuery } from "@apollo/client";
+import { useTranslations } from "next-intl";
+import html2canvas from "html2canvas";
+import { jsPDF } from "jspdf";
+
+import ProblemSubtitle from "../../components/atoms/ProblemSubtitle";
+import Tag from "../../components/atoms/Tag";
+import ScrollToTopButton from "../../components/atoms/ScrollToTopButton";
+import Button from "../../components/atoms/Button";
+
+import {
+  ExtendedProblemDetails,
+  GetProblemDetailsQuery,
+  PROBLEM_DETAILS,
+} from "./interface";
+import { formatDate } from "./utils";
+import { secondarySubtitle } from "../../styles/colors";
+import * as S from "./styles";
+import StyledButtonLink from "../../components/atoms/Button/StyledButtonLink";
+import { DownloadIcon } from "../../components/atoms/Button/styles";
+
+import { GetStaticPaths, GetStaticPropsContext } from "next";
+import { GetProblemsQuery, PROBLEMS } from "../problems-page/interface";
+import { client } from "../../apolloClient";
+
+export const getStaticPaths = async () => {
+  return {
+    paths: ["/problems/[problemId]"],
+    fallback: true,
+  };
+};
+
+// export async function getStaticPaths() {
+//   const { data } = useQuery<GetProblemsQuery>(PROBLEMS);
+//   const { problems } = data;
+//   const paths = problems.map((problem) => ({
+//     params: { problemId: problem.id },
+//   }));
+
+//   return {
+//     paths: paths || [],
+//     fallback: true,
+//   };
+// }
+
+export async function getStaticProps({
+  locale,
+  params,
+}: GetStaticPropsContext) {
+  const apolloClient = client;
+  await Promise.all([apolloClient.query({ query: PROBLEM_DETAILS })]);
+  return {
+    props: {
+      messages: (await import(`../../messages/${locale}.json`)).default,
+      initialApolloState: apolloClient.cache.extract(),
+    },
+    revalidate: 1,
+  };
+}
+
+const ProblemDetailedPage: FC = () => {
+  const t = useTranslations("problemDetailedPage");
+
+  const router = useRouter();
+  const { problemId } = router.query;
+
+  const { data, loading } = useQuery<GetProblemDetailsQuery>(PROBLEM_DETAILS, {
+    variables: { problemId },
+  });
+
+  const [problemDetails, setProblemDetails] = useState<ExtendedProblemDetails>(
+    {} as ExtendedProblemDetails
+  );
+
+  const problemsLoaded =
+    !!data?.problem?.details &&
+    Object.keys(data?.problem?.details).length > 0 &&
+    !loading;
+
+  useEffect(() => {
+    if (problemsLoaded) {
+      const { details } = data.problem;
+      setProblemDetails({
+        ...details,
+      });
+    }
+  }, [data, loading]);
+
+  const formattedDate =
+    problemDetails.createdAt && formatDate(problemDetails.createdAt);
+
+  const printRef = useRef<HTMLDivElement>(null);
+
+  const handleDownloadPdf = async () => {
+    const element = printRef.current as HTMLDivElement;
+
+    const canvas = await html2canvas(element);
+    const imgData = canvas.toDataURL("image/png");
+    const pdf = new jsPDF("p", "mm");
+
+    let position = 0;
+
+    const imgProperties = pdf.getImageProperties(imgData);
+    const pdfWidth = pdf.internal.pageSize.width - 20;
+
+    const pageHeight = 297;
+    const pdfHeight = (imgProperties.height * pdfWidth) / imgProperties.width;
+
+    let heightLeft = pdfHeight;
+
+    pdf.addImage(imgData, "PNG", 10, position, pdfWidth, pdfHeight);
+    heightLeft -= pageHeight;
+
+    while (heightLeft >= 0) {
+      position = heightLeft - pdfHeight;
+      pdf.addPage();
+      pdf.addImage(imgData, "PNG", 10, position, pdfWidth, pdfHeight);
+      heightLeft -= pageHeight;
+    }
+    pdf.save(problemDetails.title);
+  };
+
+  return (
+    <S.ProblemDetailedWrapper>
+      <S.GoToProblemsListWrapper href="/">
+        <S.Arrow />
+        <S.GoToProblemsListSpan>
+          {t("goBackToProblemsList")}
+        </S.GoToProblemsListSpan>
+      </S.GoToProblemsListWrapper>
+      <S.ProblemDetailedContent ref={printRef}>
+        <S.EducationLevel>{problemDetails.level}</S.EducationLevel>
+        <S.TitleHeading>{problemDetails.title}</S.TitleHeading>
+        <S.ProblemCreationDetailsWrapper>
+          <S.ProblemCreationDetails>
+            {problemDetails.author}
+          </S.ProblemCreationDetails>
+          <S.ProblemCreationDetails>{formattedDate}</S.ProblemCreationDetails>
+        </S.ProblemCreationDetailsWrapper>
+        <S.Section data-html2canvas-ignore="true">
+          <S.TagsWrapper>
+            {problemDetails.tags &&
+              problemDetails.tags.map((tagText: string) => (
+                <Tag text={tagText} key={tagText} />
+              ))}
+          </S.TagsWrapper>
+          <Button
+            $isPrimary={false}
+            withDownloadIcon={true}
+            $alignSelf="center"
+            onClick={handleDownloadPdf}
+          >
+            {t("downloadProblem")}
+          </Button>
+        </S.Section>
+        <S.ProblemSection>
+          <ProblemSubtitle subtitle={t("problemContent")} />
+          <S.ProblemSectionP>{problemDetails.description}</S.ProblemSectionP>
+          <S.ProblemSectionP>{problemDetails.question}</S.ProblemSectionP>
+        </S.ProblemSection>
+        <S.ProblemSection>
+          <ProblemSubtitle subtitle={t("answer")} />
+          <S.ProblemSectionP>{problemDetails.answer}</S.ProblemSectionP>
+        </S.ProblemSection>
+        <S.ProblemSection data-html2canvas-ignore="true">
+          <ProblemSubtitle subtitle={t("resources")} />
+          <StyledButtonLink
+            $isPrimary={false}
+            $alignSelf={"flex-start"}
+            href={"/problem_detailed_page.pdf"}
+            // target="_blank"
+            // download="problem_detailed_page"
+          >
+            <DownloadIcon aria-hidden title="Ikona Pobierania" />
+            {problemDetails.resources?.replace(/\s/g, `\u00A0`)}
+          </StyledButtonLink>
+        </S.ProblemSection>
+        <S.ProblemSection>
+          <ProblemSubtitle subtitle={t("openingGuidance")} />
+          <S.ProblemSectionP>
+            {problemDetails.openingGuidance}
+          </S.ProblemSectionP>
+        </S.ProblemSection>
+        <S.ProblemExtensionHeading>
+          {t("problemExtension")}
+        </S.ProblemExtensionHeading>
+        <S.ProblemSection>
+          <ProblemSubtitle
+            color={secondarySubtitle}
+            subtitle={t("problemExtensionContent")}
+          ></ProblemSubtitle>
+          <S.ProblemSectionP>
+            {problemDetails.extension?.description}
+          </S.ProblemSectionP>
+          <S.ProblemSectionP>
+            {problemDetails.extension?.question}
+          </S.ProblemSectionP>
+        </S.ProblemSection>
+        <S.ProblemSection>
+          <ProblemSubtitle
+            color={secondarySubtitle}
+            subtitle={t("answer")}
+          ></ProblemSubtitle>
+          <S.ProblemSectionP>
+            {problemDetails.extension?.answer}
+          </S.ProblemSectionP>
+        </S.ProblemSection>
+        <S.ProblemSection>
+          <ProblemSubtitle subtitle={t("guidance")}></ProblemSubtitle>
+          <S.ProblemSectionP>{problemDetails.guidance}</S.ProblemSectionP>
+        </S.ProblemSection>
+        <S.ProblemSection>
+          <ProblemSubtitle subtitle={t("closingGuidance")}></ProblemSubtitle>
+          <S.ProblemSectionP>
+            {problemDetails.closingGuidance}
+          </S.ProblemSectionP>
+        </S.ProblemSection>
+      </S.ProblemDetailedContent>
+      <ScrollToTopButton />
+    </S.ProblemDetailedWrapper>
+  );
+};
+
+export default ProblemDetailedPage;
---
+++ pages/problems/styles.tsx
@@ -0,0 +1,183 @@
+import styled from "styled-components";
+import Link from "next/link";
+
+// import { ReactComponent as ArrowLeft } from "../../../../public/assets/img/icons/arrow-left.svg";
+import { blue100, grey300 } from "../../styles/colors";
+import { minDevice } from "../../styles/breakpoints";
+import Wrapper from "../../components/atoms/Wrapper";
+
+const ProblemDetailedWrapper = styled(Wrapper)`
+  background: white;
+  flex-direction: column;
+  margin: 1.6rem 0 8rem 0;
+  min-height: 100vh;
+
+  @media ${minDevice.desktopTablet} {
+    margin-bottom: 2.1rem;
+  }
+
+  @media ${minDevice.desktopL} {
+    margin: 3.2rem 0 6.4rem 0;
+  }
+`;
+
+const GoToProblemsListWrapper = styled(Link)`
+  display: flex;
+  align-items: center;
+  color: ${blue100};
+  margin-bottom: 4.8rem;
+
+  @media ${minDevice.desktopTablet} {
+    margin-bottom: 5.6rem;
+  }
+
+  @media ${minDevice.desktopL} {
+    margin-bottom: 7.2rem;
+  }
+`;
+
+const Arrow = styled.span`
+  width: 1.6rem;
+`;
+
+const GoToProblemsListSpan = styled.span`
+  font-size: 1.4rem;
+  line-height: 2.4rem;
+  margin-left: 1.1rem;
+
+  @media ${minDevice.desktopTablet} {
+    margin-left: 0.7rem;
+  }
+
+  @media ${minDevice.desktopL} {
+    font-size: 1.6rem;
+  }
+`;
+
+const ProblemDetailedContent = styled.div``;
+
+const EducationLevel = styled.span`
+  font-size: 1.4rem;
+  line-height: 2.4rem;
+  color: ${grey300};
+  text-transform: uppercase;
+  margin-bottom: 0.8rem;
+`;
+
+const TitleHeading = styled.h1`
+  font-size: 1.8rem;
+  line-height: 2.4rem;
+  font-weight: 700;
+  margin-bottom: 2.4rem;
+
+  @media ${minDevice.desktopTablet} {
+    font-size: 2.4rem;
+    line-height: 4.8rem;
+  }
+`;
+
+const ProblemCreationDetailsWrapper = styled.div`
+  display: flex;
+
+  @media ${minDevice.desktopXL} {
+    margin-bottom: 1.6rem;
+  }
+`;
+
+const ProblemCreationDetails = styled(EducationLevel)`
+  text-transform: none;
+  display: inline;
+
+  &:first-child {
+    margin-right: 1rem;
+    position: relative;
+
+    &::after {
+      content: "•";
+      position: absolute;
+      margin-left: 0.8rem;
+    }
+  }
+  &:nth-child(2) {
+    padding-left: 1rem;
+  }
+`;
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-bottom: 4.8rem;
+
+  @media ${minDevice.desktopTablet} {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-start;
+    margin-bottom: 7.2rem;
+  }
+`;
+
+const TagsWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+  margin-bottom: 4rem;
+
+  @media ${minDevice.desktopTablet} {
+    margin-bottom: 0;
+  }
+`;
+
+const ProblemSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 3.2rem;
+
+  @media ${minDevice.desktopTablet} {
+    margin-bottom: 5.6rem;
+  }
+`;
+
+const ProblemSectionP = styled.p`
+  font-size: 1.4rem;
+  line-height: 2.4rem;
+
+  @media ${minDevice.desktopTablet} {
+    font-size: 1.6rem;
+    line-height: 3.2rem;
+  }
+  & + p {
+    margin-top: 1.6rem;
+  }
+`;
+
+const ProblemExtensionHeading = styled.h1`
+  font-size: 2rem;
+  line-height: 3.2rem;
+  font-weight: 700;
+  text-transform: uppercase;
+  margin-bottom: 1.6rem;
+
+  @media ${minDevice.desktopTablet} {
+    font-size: 2.4rem;
+    line-height: 4.8rem;
+    margin-bottom: 2.4rem;
+  }
+`;
+
+export {
+  ProblemDetailedWrapper,
+  GoToProblemsListWrapper,
+  Arrow,
+  GoToProblemsListSpan,
+  ProblemDetailedContent,
+  EducationLevel,
+  TitleHeading,
+  ProblemCreationDetailsWrapper,
+  ProblemCreationDetails,
+  Section,
+  TagsWrapper,
+  ProblemSection,
+  ProblemSectionP,
+  ProblemExtensionHeading,
+};
